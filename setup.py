04d759fae519bc60b666f20f3e3cba5a767f6dbb
amit-15 <amitkumartiwaree.akt@gmailcom>
2022-07-19 15:52:00 +0530

    setup done

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="src",
+    version="0.0.1",
+    description="its a wine Q package", 
+    author="amit-15", 
+    packages=find_packages(),
+    license="MIT"
+)
+
